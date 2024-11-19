c1f2802b064c816851813fdf65b906caa8453c9a
Morg92b <morgan.bouaziz@outlook.fr>
2024-11-19 10:35:21 +0100

    return dataview

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-    let buffer = new ArrayBuffer(length);
-    let Int8 = new Int8Array(buffer);
+  const buffer = new ArrayBuffer(length);
+  const Int8 = new Int8Array(buffer);
 
-    if (position < length) {
-        Int8[position] = value;
-    }
-    else {
-       throw new Error("Position outside range")
-    }
-    return buffer;
+  if (position < length) {
+    Int8[position] = value;
+  } else {
+    throw new Error('Position outside range');
+  }
+  return new DataView(buffer);
 }
