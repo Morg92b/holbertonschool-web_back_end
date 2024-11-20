3c3a70735ec44371668d4dc997597d0995ccbb60
Morg92b <morgan.bouaziz@outlook.fr>
2024-11-20 17:32:31 +0100

    fix synthax

--- ES6_promise/7-load_balancer.js
+++ ES6_promise/7-load_balancer.js
@@ -1,4 +1,4 @@
 export default function loadBalancer(chinaDownload, USDowload) {
-    return Promise.race([chinaDownload, USDowload])
-     .then((value) => value);
+  return Promise.race([chinaDownload, USDowload])
+    .then((value) => value);
 }
