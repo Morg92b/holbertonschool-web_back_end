de84c59ae22ead0a724b817c053c020e22349764
Morg92b <morgan.bouaziz@outlook.fr>
2024-11-20 17:22:52 +0100

    fix handle with async

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,14 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(filename),
-  ]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+    uploadPhoto(fileName),
+  ]);
+
+  return results.map(({ status, value, reason }) => ({
+    status,
+    value: status === 'fulfilled' ? value : reason.toString(),
+  }));
 }
