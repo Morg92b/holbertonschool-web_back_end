c4c0d478013bb38e9381517c1b4675d7f52054e6
Morg92b <morgan.bouaziz@outlook.fr>
2025-01-20 13:20:44 +0100

    add commentary

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,5 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?');
 
+// That function dynamic for what ur names
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
   console.log(`Your name is: ${name}`);
